19c3e29b8b126ab1969e68d17c86ef9dd430a14e
unknown <candianirfernando@gmail.com>
2021-08-03 21:15:07 -0300

    Inicio programação de JavaScript

---
+++ JavaScript- Programando na linguagem da web/1- Introdução/introducao javascript-Adicionando arquivo externo/js/principal.js
@@ -0,0 +1,8 @@
+
+console.log("Este arquivo foi carregado externo");
+
+var titulo = document.querySelector(".titulo");
+console.log(titulo.textContent);
+
+titulo.textContent = "Teste com textContent";
+
